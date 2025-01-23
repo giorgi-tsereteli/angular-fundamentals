a9aa4aada4d21a333e21b83b55fd2260a63592b2
Giorgi Tsereteli <giorgi.webdev@gmail.com>
2025-01-22 22:44:30 -0800

    Implement routing for home, catalog, and cart components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+
+const routes: Routes = [
+  {path: 'home', component:HomeComponent, title: 'Joes Home Page'},
+  {path: 'catalog/:filter', component:CatalogComponent, title: 'Catalog Page'},
+  {path: 'cart', component:CartComponent, title: 'Cart Page'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { IProduct } from './product.model';
 import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -14,19 +15,33 @@ export class CatalogComponent {
 
   constructor(
     private cartSvc: CartService,
-    private productSvc: ProductService
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute // to filter page of products
   ) { }
 
   ngOnInit() {
     this.productSvc.getProducts().subscribe((products) => {
       this.products = products;
     });
+    // Subscribe to route parameters
+    this.route.params.subscribe((params) => {
+      // Update the filter property with the 'filter' parameter from the route
+      this.filter = params['filter'] ?? ''
+      // 'subscribe' listens for changes in the route parameters and executes the callback function when they change
+    });
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']); // conditional navigation
   }
 
+  /**
+   * Filters the products based on the selected category.
+   * If no category is selected (filter is an empty string), it returns all products.
+   * Otherwise, it returns only the products that match the selected category.
+   */
   getFilteredProducts() {
     return this.filter === ''
       ? this.products
